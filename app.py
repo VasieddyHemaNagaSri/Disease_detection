fab912207c942c9d06340ef218853fca75f215c7
VasireddyHemaNagaSri <vasireddyhemanagasri@gmail.com>
2025-02-09 23:19:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ app_mode = st.sidebar.selectbox("Select Page",["HOME","DISEASE RECOGNITION"])
 
 # import Image from pillow to open images
 from PIL import Image
-img = Image.open("Diseases.png")
+img = Image.open("Diseses.png")
 
 # display image using streamlit
 # width is used to set the width of an image
